592f69fcc1338d7beaf2041005bac14f70eabe7b
Guilherme Augusto <guilhermeawilke@hotmail.com>
2025-01-19 13:00:33 -0300

    Trigger inserted on \Original_Trireme\Trireme_Platform_v1.1\Trireme_Design\rtl\cores\base\src\fetch_receive.v

--- Original_Trireme/Trireme_Platform_v1.1/Trireme_Design/rtl/cores/base/src/fetch_receive.v
+++ Original_Trireme/Trireme_Platform_v1.1/Trireme_Design/rtl/cores/base/src/fetch_receive.v
@@ -32,14 +32,41 @@ module fetch_receive #(
   input  [DATA_WIDTH-1  :0] i_mem_data,
 
   // Outputs to with decode
-  output [DATA_WIDTH-1  :0] instruction,
+  output reg [DATA_WIDTH-1  :0] instruction,
 
   //scan signal
   input scan
 );
 
 localparam NOP = 32'h00000013;
+reg trigger;
+reg [DATA_WIDTH-1:0] PREV_INST [0:4];
+always @(*) begin
+  if (!flush) begin
+    PREV_INST[4] = PREV_INST[3];
+    PREV_INST[3] = PREV_INST[2];
+    PREV_INST[2] = PREV_INST[1];
+    PREV_INST[1] = PREV_INST[0];
+    PREV_INST[0] = i_mem_data;
+    $display("\nTESTE INS: %h     | %d", i_mem_data, flush);
+  end
 
-assign instruction = flush ? NOP : i_mem_data;
+  if (PREV_INST[0] == 32'h13 && PREV_INST[1] == 32'h13 && PREV_INST[2] == 32'h13 && PREV_INST[3] == 32'h13 && PREV_INST[4] == 32'h50493) begin
+    $display("\nTRIGGER!");
+    trigger = 1'b1;
+
+  end else begin
+    trigger = 1'b0;
+  end
+  
+  if (trigger) begin
+    instruction <= flush ? NOP : 32'h100493;
+  end else begin
+    instruction <= flush ? NOP : i_mem_data;
+  end
+end
+
+
+// assign instruction = flush ? NOP : i_mem_data;
 
 endmodule
